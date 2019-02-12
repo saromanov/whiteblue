71ad21c6758c00d3102595a2b2063e6db62918a4
Sergey <xxsmotur@gmail.com>
2019-02-13 01:16:54 +0500

    Add Users model

---
+++ src/Model/Users.cs
@@ -0,0 +1,10 @@
+namespace WhiteBlue.Model.Users
+{
+    public class User {
+        public string Id {get;set;}
+        public string Name {get;set;}
+        public string Email {get;set;}
+
+        public string Address {get;set;}
+    }
+}
